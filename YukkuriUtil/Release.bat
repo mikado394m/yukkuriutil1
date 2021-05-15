58074f913512f08bbc7c83eabae022d993c26c4c
oriaca <oriaca372m@outlook.jp>
2021-05-16 01:52:29 +0900

    v0.0.10.10

--- YukkuriUtil/Release.bat
+++ YukkuriUtil/Release.bat
@@ -5,12 +5,9 @@ setlocal
 rmdir /s /q Release
 mkdir Release
 
-copy bin\Release\Livet.dll Release
-copy bin\Release\Microsoft.Expression.Interactions.dll Release
-copy bin\Release\NAudio.dll Release
-copy bin\Release\System.Windows.Interactivity.dll Release
+copy bin\Release\*.dll Release
 copy bin\Release\YukkuriUtil.exe Release
-copy Resources\ReadMe.txt Release
+copy ..\ReadMe.txt Release
 copy ..\LICENSE.txt Release
 
 mkdir Release\softalk
