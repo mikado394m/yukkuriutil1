f8aa9d6a8bb9c76209c2b1712cc2a57ba06f0d96
oriaca <oriaca372m@outlook.jp>
2021-05-29 23:56:54 +0900

    Fix #6

---
+++ YukkuriUtil/Views/IMETextBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace YukkuriUtil.Views
+{
+    // http://denasu.com/blog/2015/06/diary3087
+    public class IMETextBox : TextBox
+    {
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            GetBindingExpression(TextProperty).UpdateSource();
+            base.OnTextChanged(e);
+        }
+    }
+}
